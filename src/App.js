c2e87cde854fdf0bf65b442ac01345dc103c19fa
marcinmoskala <marcinmoskala@gmail.com>
2023-03-09 15:02:05 +0100

    Make cookie test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        (async () => {
+          const res0 = await fetch("https://ktor-playground.herokuapp.com/respond_cookie", {
+            "method": "GET",
+            "credentials": "include"
+          })
+          console.log(await res0.text())
+            const res1 = await fetch("https://ktor-playground.herokuapp.com/set_cookie", {
+                "method": "GET",
+                "credentials": "include",
+                referrerPolicy: "unsafe-url"
+            })
+            console.log(res1)
+            console.log(await res1.text())
+            const res2 = await fetch("https://ktor-playground.herokuapp.com/respond_cookie", {
+                "method": "GET",
+                "credentials": "include"
+            })
+            console.log(res2)
+            console.log(await res2.text())
+
+        })()
+    }, [])
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
