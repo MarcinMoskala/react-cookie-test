a341c9512f77ec5fd1491b6cccd9cec54895ed53
marcinmoskala <marcinmoskala@gmail.com>
2023-03-09 15:57:27 +0100

    Test passing cookie to websocket

--- src/App.js
+++ src/App.js
@@ -2,28 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 import {useEffect} from "react";
 
+const BASE_URL = "https://ktor-playground.herokuapp.com"
+const WS_BASE_URL = "wss://ktor-playground.herokuapp.com"
+
 function App() {
     useEffect(() => {
         (async () => {
-          const res0 = await fetch("https://ktor-playground.herokuapp.com/respond_cookie", {
-            "method": "GET",
-            "credentials": "include"
-          })
-          console.log(await res0.text())
-            const res1 = await fetch("https://ktor-playground.herokuapp.com/set_cookie", {
+            const res0 = await fetch(BASE_URL + "/respond_cookie", {
+                "method": "GET",
+                "credentials": "include"
+            })
+            console.log(await res0.text())
+            const res1 = await fetch(BASE_URL + "/set_cookie", {
                 "method": "GET",
                 "credentials": "include",
-                referrerPolicy: "unsafe-url"
             })
             console.log(res1)
             console.log(await res1.text())
-            const res2 = await fetch("https://ktor-playground.herokuapp.com/respond_cookie", {
+            const res2 = await fetch(BASE_URL + "/respond_cookie", {
                 "method": "GET",
                 "credentials": "include"
             })
             console.log(res2)
             console.log(await res2.text())
-
+            const socket = new WebSocket(WS_BASE_URL + "/sample_socket")
+            socket.onmessage = (event) => {
+                console.log(event.data);
+            }
+            socket.onopen = function (e) {
+                console.log("Connection established");
+                console.log(e);
+            };
+            socket.onclose = function (e) {
+                console.log("Connection closed");
+                console.log(e);
+            };
+            socket.onerror = function (e) {
+                console.log("Error");
+                console.log(e);
+            };
         })()
     }, [])
     return (
